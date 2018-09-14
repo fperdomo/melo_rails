9a88755f564471e608b34eaeb4738d6f1c7b9cfe
F. Perdomo <perdomofacundo@hotmail.com>
2018-09-14 18:00:58 -0300

    Sesson 34 and 35

--- blog/app/helpers/application_helper.rb
+++ blog/app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	def article_cover url, options ={}
+
+		html_class = options[:class]
+		html_style = "background: url(#{url});top:0px;left:0px;width: 100%; height: 400px;";
+
+		html = "<header class='#{html_class}' style='#{html_style}'>"\
+			"</header>"
+
+		html.html_safe
+	end
 end
--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -8,4 +8,14 @@ class User < ApplicationRecord
   has_many :comments
 
   include PermissionsConcern
+
+  def avatar
+  	email_address = self.email.downcase
+
+  	#create the md5 hash
+  	hash = Digest::MD5.hexdigest(email_address)
+
+  	#compile URL which can be used in <img
+  	image_src = "http://www.gravatar.com/avatar/#{hash}"
+  end
 end
